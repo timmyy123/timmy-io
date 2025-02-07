5742da38f042e4ce4f6ad7dd2dc672f166385757
timmyy123 <164847362+timmyy123@users.noreply.github.com>
2025-02-07 19:29:43 +1100

    Refactor Info component layout and styles, update global CSS utilities

--- app/page.tsx
+++ app/page.tsx
@@ -20,6 +20,7 @@ export default function Home() {
         <Info/>
         </div>
       </section>
+      
     </main>
   );
 }
--- components/Info.tsx
+++ components/Info.tsx
@@ -6,31 +6,31 @@ import { MdEmail } from "react-icons/md";
 const Info = () => {
   return (
     <div className="aspect-2/1 p-4 flex  ">
-      <div className=" h-full flex flex-col justify-center ">
-        <div className="mb-4">
+      <div className=" h-full flex flex-col justify-center grow-1 ">
           <h1 className="text-3xl mb-2">Education</h1>
           <p className="text-xl">Master of Information Technology</p>
           <p className="text-xl">Monash University, AU</p>
-          <p className="text-xl">Jul 2023 - Jul 2025</p>
-        </div>
-        <div>
+          <p className="text-xl">Jul 2023 - Jul 2025</p>      
+      </div>
+      <div className=" h-full flex flex-col justify-center grow-1  ">
+        
           <h1 className="text-3xl mb-2">Contacts</h1>
           <div className="flex gap-2 items-center">
             <FaPhoneAlt />
             <p>{process.env.PHONE_NUMBER}</p>
           </div>
-          <div className="flex gap-2 items-center">
+          <div className="flex gap-2 items-center ">
             <MdEmail />
             <p>{process.env.EMAIL}</p>
           </div>
-        </div>
+
       </div>
-      <div className="card-top flex-grow flex-col ms-2 justify-center items-center gap-2">
+      <div className=" flex flex-col  justify-center items-center gap-2 grow-1">
         <Link href={process.env.GITHUB || "https://github.com"}>
-          <FaGithub className="w-25 h-25"/>
+          <FaGithub className="w-25 h-25" />
         </Link>
         <Link href={process.env.LINKEDIN || "https://www.linkedin.com"}>
-          <FaLinkedin className="w-25 h-25"/>
+          <FaLinkedin className="w-25 h-25" />
         </Link>
       </div>
     </div>
